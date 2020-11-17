417cbc543cf19d51fc76a54394f536cff7d74f88
Alec Scully <ScullyAlec@gmail.com>
2020-11-17 17:58:15 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
+class Book
+    attr_accessor :author, :page_count, :genre
+    #the above code is the simplified code below with author
+    attr_reader :title
+    #the above code is the simplified code below for title setting.
 
+    def initialize(title)
+        @title = title
+    end
 
+    # def title
+    #     @title
+    # end
+
+    # def author=(author)
+    #     @author = author
+    # end
+
+    # def author
+    #     @author
+    # end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end 
+end
